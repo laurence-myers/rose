18efcc3aaf4fbd004408347a3cfee4ebff55b54b
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2016-10-30 13:34:55 +1100

    Rename T... interfaces to ...Row

--- src/templates/table.ts
+++ src/templates/table.ts
@@ -3,7 +3,7 @@ import {POSTGRES_TO_TYPESCRIPT_TYPE_MAP} from "../dbtypes";
 
 export function TableTemplate(tableMetadata : TableMetadata) : string {
 	const sb : string[] = [];
-	sb.push(`export interface T${ tableMetadata.name } {\n`);
+	sb.push(`export interface ${ tableMetadata.name }Row {\n`);
 	for (let column of tableMetadata.columns.values()) {
 		sb.push(`\t${ column.name }${ column.isNullable ? '?' : '' } : ${ POSTGRES_TO_TYPESCRIPT_TYPE_MAP.get(column.type) };\n`);
 	}
