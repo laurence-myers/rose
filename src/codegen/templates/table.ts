aaae9c4bc7d1353b83499b3071c7fd1b671b573e
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2017-02-07 09:23:55 +1100

    Just use string concatenation instead of a string array when constructing templates. Might perform better in V8 in most cases (thanks to "strings actually using a "rope" data structure.

--- src/codegen/templates/table.ts
+++ src/codegen/templates/table.ts
@@ -2,11 +2,11 @@ import {TableMetadata} from "../dbmetadata";
 import {POSTGRES_TO_TYPESCRIPT_TYPE_MAP} from "../dbtypes";
 
 export function TableTemplate(tableMetadata : TableMetadata) : string {
-	const sb : string[] = [];
-	sb.push(`export interface ${ tableMetadata.name }Row {\n`);
+	let sb : string = '';
+	sb += `export interface ${ tableMetadata.name }Row {\n`;
 	for (let column of tableMetadata.columns.values()) {
-		sb.push(`\t${ column.name }${ column.isNullable ? '?' : '' } : ${ POSTGRES_TO_TYPESCRIPT_TYPE_MAP.get(column.type) };\n`);
+		sb += `\t${ column.name }${ column.isNullable ? '?' : '' } : ${ POSTGRES_TO_TYPESCRIPT_TYPE_MAP.get(column.type) };\n`;
 	}
-	sb.push(`}\n`);
-	return sb.join('');
+	sb += `}\n`;
+	return sb;
 }
--- src/codegen/templates/tableMetamodel.ts
+++ src/codegen/templates/tableMetamodel.ts
@@ -1,24 +1,24 @@
 import {TableMetadata} from "../dbmetadata";
 
 export function TableMetamodelTemplate(tableMetadata : TableMetadata) : string {
-	const sb : string[] = [];
+	let sb : string = '';
 
-	sb.push(`export type ${ tableMetadata.name }Column = (\n`);
+	sb += `export type ${ tableMetadata.name }Column = (\n`;
 	let numElements = 0;
 	for (let column of tableMetadata.columns.values()) {
-		sb.push('\t');
+		sb += '\t';
 		if (numElements) {
-			sb.push('| ');
+			sb += '| ';
 		}
-		sb.push(`'${ column.name }'\n`);
+		sb += `'${ column.name }'\n`;
 		numElements++;
 	}
-	sb.push(`);\n\n`);
+	sb += `);\n\n`;
 
-	sb.push(`export const ${ tableMetadata.name }Columns = Object.freeze({\n`);
+	sb += `export const ${ tableMetadata.name }Columns = Object.freeze({\n`;
 	for (let column of tableMetadata.columns.values()) {
-		sb.push(`\t'${ column.name }': '${ column.name }',\n`);
+		sb += `\t'${ column.name }': '${ column.name }',\n`;
 	}
-	sb.push(`});\n`);
-	return sb.join('');
+	sb += `});\n`;
+	return sb;
 }
