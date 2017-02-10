8b3232ac0a91212b3bceec236e71e5bb47460175
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2017-02-11 00:48:46 +1100

    Refactor tableMetamodel sections to separate functions

--- src/codegen/templates/tableMetamodel.ts
+++ src/codegen/templates/tableMetamodel.ts
@@ -1,8 +1,7 @@
 import {TableMetadata} from "../dbmetadata";
 
-export function TableMetamodelTemplate(tableMetadata : TableMetadata) : string {
+function writeColumnType(tableMetadata : TableMetadata) {
 	let sb : string = '';
-
 	sb += `export type ${ tableMetadata.name }Column = (\n`;
 	let numElements = 0;
 	for (let column of tableMetadata.columns.values()) {
@@ -14,11 +13,24 @@ export function TableMetamodelTemplate(tableMetadata : TableMetadata) : string {
 		numElements++;
 	}
 	sb += `);\n\n`;
+	return sb;
+}
 
+function writeColumns(tableMetadata : TableMetadata) {
+	let sb = '';
 	sb += `export const ${ tableMetadata.name }Columns = Object.freeze({\n`;
 	for (let column of tableMetadata.columns.values()) {
 		sb += `\t'${ column.name }': '${ column.name }',\n`;
 	}
-	sb += `});\n`;
+	sb += `});\n\n`;
+	return sb;
+}
+
+export function TableMetamodelTemplate(tableMetadata : TableMetadata) : string {
+	let sb : string = '';
+
+	sb += writeColumnType(tableMetadata);
+	sb += writeColumns(tableMetadata);
+
 	return sb;
 }
