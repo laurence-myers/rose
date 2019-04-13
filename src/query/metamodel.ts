5e13ba0a4945552f2a4883e75a5e98c1c6126d68
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2019-04-14 09:52:58 +1000

    Remove unused decorators

--- src/query/metamodel.ts
+++ src/query/metamodel.ts
@@ -1,40 +1,16 @@
 import "reflect-metadata";
-import {InvalidTableDefinitionError, InvalidColumnDefinitionError} from "../errors";
+import {InvalidColumnDefinitionError, InvalidTableDefinitionError} from "../errors";
 import {getMetadata} from "../lang";
 import {
-	ColumnReferenceNode, ConstantNode, OrderByExpressionNode,
-	BooleanBinaryOperationNode, BooleanUnaryOperationNode, SubSelectNode, FunctionExpressionNode, ExpressionListNode,
-	ParameterOrValueExpressionNode, ValueExpressionNode
+	BooleanBinaryOperationNode,
+	BooleanUnaryOperationNode,
+	ColumnReferenceNode,
+	ConstantNode,
+	OrderByExpressionNode,
+	ParameterOrValueExpressionNode,
+	ValueExpressionNode
 } from "./ast";
-import {row} from "./dsl";
 import {any} from "./postgresql/functions/array/functions";
-import {SelectorKind} from "./querySelector";
-
-export const METADATA_KEY_PREFIX = "arbaon.";
-export const TABLE_METADATA_KEY = `${ METADATA_KEY_PREFIX }table`;
-export function Table<T>(metamodel : TableMetamodel) : ClassDecorator {
-	return function (target : Function) {
-		if (Reflect.hasMetadata(TABLE_METADATA_KEY, target)) {
-			throw new InvalidTableDefinitionError(`Class "${ target.name }" already has table metadata defined.`);
-		} else {
-			Reflect.defineMetadata(TABLE_METADATA_KEY, metamodel, target.prototype);
-		}
-	}
-}
-
-export const SELECT_METADATA_KEY = `${ METADATA_KEY_PREFIX }select`;
-export function Column<T>(metamodel : ColumnMetamodel<any>) : PropertyDecorator {
-	return function (target : Object, propertyKey : string | symbol) {
-		let metadata = getMetadata<Map<string, ColumnMetamodel<any>>>(SELECT_METADATA_KEY, target);
-		if (!metadata) {
-			metadata = new Map<string, ColumnMetamodel<any>>();
-			Reflect.defineMetadata(SELECT_METADATA_KEY, metadata, target);
-		} else if (metadata.get(<string> propertyKey) !== undefined) {
-			throw new InvalidColumnDefinitionError(`Property "${ String(propertyKey) }" already has column metadata defined.`);
-		}
-		metadata.set(<string> propertyKey, metamodel);
-	}
-}
 
 export class TableMetamodel {
 
