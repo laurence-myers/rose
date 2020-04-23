d133e4b5b181c079692cda6ab6f27443ca6ff150
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2020-04-23 15:04:32 +1000

    rose-cli: dbtypes.ts: sort map entries alphabetically.

--- rose-cli/src/codegen/dbtypes.ts
+++ rose-cli/src/codegen/dbtypes.ts
@@ -3,29 +3,33 @@ import { TypeMapEntry } from "../config";
 
 const POSTGRES_TO_TYPESCRIPT_TYPE_MAP: Map<string, string> = new DefaultMap<string, string>(() => "any", <[string, string][]>[
 	["bigint", "string"], // 8 bytes, can't be represented as a FP number
-	["int8", "string"], // 8 bytes, can't be represented as a FP number
 	["bigserial", "string"], // 8 bytes, can't be represented as a FP number
 	["bit", "number"], // bit string?
-	["varbit", "number"], // bit string?
-	["boolean", "boolean"],
 	["bool", "boolean"],
+	["boolean", "boolean"],
 	["box", "boolean"],
-	["bytea", "Buffer"],
-	["character", "string"],
-	["char", "string"],
 	["bpchar", "string"],
+	["bytea", "Buffer"],
+	["char", "string"],
 	["character varying", "string"],
-	["varchar", "string"],
+	["character", "string"],
 	["cidr", "string"],
 	["circle", "any"], // ?
 	["date", "Date"],
+	["daterange", "any"], // ?
+	["decimal", "number"],
 	["double precision", "number"],
+	["float4", "number"],
 	["float8", "number"],
 	["hstore", "{ [key : string] : string }"],
 	["inet", "string"],
-	["integer", "number"],
 	["int", "number"],
+	["int2", "number"],
 	["int4", "number"],
+	["int4range", "any"], // ?
+	["int8", "string"], // 8 bytes, can't be represented as a FP number
+	["int8range", "any"], // ?
+	["integer", "number"],
 	["interval", "any"], // ?
 	["json", "object"], // would be nice to make this more explicit
 	["jsonb", "object"], // would be nice to make this more explicit
@@ -34,39 +38,34 @@ const POSTGRES_TO_TYPESCRIPT_TYPE_MAP: Map<string, string> = new DefaultMap<stri
 	["macaddr", "string"],
 	["money", "number"], // ?
 	["numeric", "number"],
-	["decimal", "number"],
+	["numrange", "any"], // ?
 	["path", "any"], // ?
 	["point", "any"], // ?
 	["polygon", "any"], // ?
 	["real", "number"],
-	["float4", "number"],
-	["smallint", "number"],
-	["int2", "number"],
-	["smallserial", "number"],
-	["serial2", "number"],
 	["serial", "number"],
+	["serial2", "number"],
 	["serial4", "number"],
-	["text", "string"],
-	["time", "Date"],
-	["time without time zone", "Date"],
-	["time with time zone", "Date"],
-	["timetz", "Date"],
-	["timestamp", "Date"],
-	["timestamp without time zone", "Date"],
-	["timestamp with time zone", "Date"],
-	["timestamptz", "Date"],
-	["tsquery", "any"], // ?
-	["tsvector", "string"], // e.g. "'bird':1 'bore':4 'california':18 'dog':16 'face':14 'must':13 'perdit':2 'pioneer':11 'stori':5 'woman':8"
+	["smallint", "number"],
+	["smallserial", "number"],
 	["snapshot", "any"], // ?
-	["uuid", "string"], // ?
-	["xml", "string"], // ?
-
-	["int4range", "any"], // ?
-	["int8range", "any"], // ?
-	["numrange", "any"], // ?
+	["text", "string"],
+	["time with time zone", "Date"],
+	["time without time zone", "Date"],
+	["time", "Date"],
+	["timestamp with time zone", "Date"],
+	["timestamp without time zone", "Date"],
+	["timestamp", "Date"],
+	["timestamptz", "Date"],
+	["timetz", "Date"],
+	["tsquery", "any"], // ?
 	["tsrange", "any"], // ?
 	["tstzrange", "any"], // ?
-	["daterange", "any"] // ?
+	["tsvector", "string"], // e.g. "'bird':1 'bore':4 'california':18 'dog':16 'face':14 'must':13 'perdit':2 'pioneer':11 'stori':5 'woman':8"
+	["uuid", "string"], // ?
+	["varbit", "number"], // bit string?
+	["varchar", "string"],
+	["xml", "string"], // ?
 
 	// Not supported; other internal formats like
 ]);
