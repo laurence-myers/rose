3d0ac103a23591f53f198bfeb18a908549f52936
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2020-07-11 12:41:45 +1000

    rose-docs: convert files in parallel/async fashion.

--- rose-docs/src/index.ts
+++ rose-docs/src/index.ts
@@ -28,26 +28,34 @@ async function walk(dir: string): Promise<string[]> {
     return files.reduce((all: string[], folderContents: string | string[]): string[] => all.concat(folderContents), []);
 }
 
+async function processFile(file: string): Promise<void> {
+    console.log(`Converting ${ file }...`);
+    const output = await asciidoctor()
+        .convert(await fs.readFile(file), {
+            backend: 'html',
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            base_dir: path.dirname(file),
+            safe: 'UNSAFE',
+        });
+    const outPath = file.replace(/([\\/])docs[\\/]/, '$1dist$1').replace(/\.adoc$/, '.html');
+    await fs.mkdir(path.dirname(outPath), {
+        recursive: true
+    });
+    await fs.writeFile(outPath, output);
+}
+
+function mapP<T, U>(values: T[], mapper: (value: T) => Promise<U>): Promise<U[]> {
+    return Promise.all(
+        values.map(mapper)
+    );
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function main(_args: string[]): Promise<ExitCode> {
     const exitCode = ExitCode.Okay;
 
     const files = await walk(path.join(arp.path, 'docs'));
-    for (const file of files) {
-        console.log(`Converting ${ file }...`);
-        const output = await asciidoctor()
-            .convert(await fs.readFile(file), {
-                backend: 'html',
-                // eslint-disable-next-line @typescript-eslint/camelcase
-                base_dir: path.dirname(file),
-                safe: 'UNSAFE'
-            });
-        const outPath = file.replace(/([\\/])docs[\\/]/, '$1dist$1').replace(/\.adoc$/, '.html');
-        await fs.mkdir(path.dirname(outPath), {
-            recursive: true
-        });
-        await fs.writeFile(outPath, output);
-    }
+    await mapP(files, processFile);
 
     return exitCode;
 }
