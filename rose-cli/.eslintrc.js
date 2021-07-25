01a3ce2b1c431dc2b232f7958b3df0328c434142
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2021-07-25 18:56:42 +1000

    rose-cli: fix eslint config to remove some formatting rules

--- rose-cli/.eslintrc.js
+++ rose-cli/.eslintrc.js
@@ -14,12 +14,7 @@ module.exports = {
 		"prettier",
 	],
 	rules: {
-		"@typescript-eslint/type-annotation-spacing": "warn",
 		"@typescript-eslint/no-for-in-array": "error",
-		"no-multiple-empty-lines": "warn",
-		"no-multi-spaces": "warn",
-		"object-curly-spacing": ["warn", "always"],
-		"template-curly-spacing": ["warn", "always"],
 		"unused-imports/no-unused-imports-ts": "warn",
 	},
 };
